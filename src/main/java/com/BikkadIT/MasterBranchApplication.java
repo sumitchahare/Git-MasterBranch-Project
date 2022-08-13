ee6f96778f3949c0596f7d95c1f8e889a5ac4a2d
sumitchahare <chaharesumit@gmail.com>
2022-08-13 15:11:54 +0530

    Master Branch Project added in central repository

---
+++ src/main/java/com/BikkadIT/MasterBranchApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterBranchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterBranchApplication.class, args);
+	}
+
+}
